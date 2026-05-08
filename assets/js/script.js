9a0b013487bdb823d4a819fc499266d31b2511d2
nyanumba260 <nyanumbadalmas02@gmail.com>
2026-05-08 16:19:01 +0300

    added JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
 console.log("Hello, World!")
+
+// variable declaration
+let First_name ="Dalmas"
+console.log(First_name)
+
+let age = 15
+console.log(age)
+
+let mpesa_balance = 370.75
+console.log(mpesa_balance)
+
+let email_address = "nyanumbadalmas02@gmail.com"
+console.log(email_address)
+
+
+let cake = "Straight of hormuz officially open for business"
+console.log(cake)
+
+let total = 5+3
+console.log(total)
+
+
+if (age >= 18) {
+    console.log(" an adult.")
+}
+else if (age >= 18) {
+    console.log(" minor.")
+}
+else {
+    console.log(" minor.")
+}
+function greet() {
+    console.log("Hello, World Iam learning Js!")
+    alert("Goodbye class!")
+}
+greet()
+
